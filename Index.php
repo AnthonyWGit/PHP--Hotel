5dba08a092230d77cae3f035eea6595450e00cba
Anthony <anthony.wetzstein@gmail.com>
2023-03-24 11:38:33 +0100

    Instancier un nouvel hotel, sortir la ville

--- Index.php
+++ Index.php
@@ -3,4 +3,6 @@ require_once('Client.php');
 require_once('Hotel.php');
 $result = "";
 $marieGeorges = new Client("Georges","Marie","50", "Femme");
+$Ibis = new Hotel("Ibis", "Rue de la Fonderie", "9", "67540", "Oswtald",$marieGeorges);
 echo $marieGeorges ->getNomClient();
+echo $Ibis->getVille();
